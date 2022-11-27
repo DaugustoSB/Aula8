41905dfda3641feddc70ea526b78d0ffee4b61f6
DaugustoSB <101142197+DaugustoSB@users.noreply.github.com>
2022-11-27 11:55:55 -0300

    Add files via upload

---
+++ PilhaTeste.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mainaula8;
+
+
+public class PilhaTeste {
+    int[] pilhaP = new int[1000];
+    int[] pilhaN = new int[1000];
+}
